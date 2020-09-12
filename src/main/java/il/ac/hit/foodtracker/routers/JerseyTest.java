163c90a33bc218e0003388e852de5daf60ec426b
Galcarmi <carmigalc@gmail.com>
2020-09-12 22:02:07 +0300

    first commit, init project structure

---
+++ src/main/java/il/ac/hit/foodtracker/routers/JerseyTest.java
@@ -0,0 +1,16 @@
+package il.ac.hit.foodtracker.routers;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class JerseyTest {
+//
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String sayHtmlHello() {
+		return "Hello from Jerseyyy";
+	}
+}
